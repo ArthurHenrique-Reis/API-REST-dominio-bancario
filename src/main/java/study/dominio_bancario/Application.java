316e0904bcfe342d9992211979ba92fb69eec810
ArthurHenrique-Reis <carvalhoarthurreis@yahoo.com.br>
2024-09-16 14:24:26 -0300

    Initial commit

---
+++ src/main/java/study/dominio_bancario/Application.java
@@ -0,0 +1,13 @@
+package study.dominio_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioBancarioApplication.class, args);
+	}
+
+}
